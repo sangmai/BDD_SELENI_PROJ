1d10416fb2ae67419c730e569c35f085dd216425
vodanhdn <maisang90dn@gmail.com>
2017-04-18 16:25:35 +0700

    update excel report

--- src/main/java/common/ExcelReportListener.java
+++ src/main/java/common/ExcelReportListener.java
@@ -4,99 +4,126 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.testng.IInvokedMethod;
 import org.testng.IInvokedMethodListener;
 import org.testng.ITestResult;
 
 public class ExcelReportListener implements IInvokedMethodListener {
-	Map<String, String> testResultData = new HashMap<String, String>();
-	Map<String, String> exceptionResult = new HashMap<String, String>();
-	Map<String, String> timeToRunResult = new HashMap<String, String>();
+	Map<String, ArrayList<String>> allDataAfterTest = new HashMap<String, ArrayList<String>>();
 	private static HSSFWorkbook excelBook;
 	private static HSSFSheet excelSheet;
+	private static final Log log = LogFactory.getLog(ExcelReportListener.class);
 
 	@Override
 	public void afterInvocation(IInvokedMethod method, ITestResult result) {
 		// TODO Auto-generated method stub
+		ArrayList<String> dataAfterTest = new ArrayList<String>();
 		String timeStartRun = getTime(result.getStartMillis());
-		String timeToRun = timeStartRun + " Take time: " + String
-				.valueOf(TimeUnit.MILLISECONDS.toSeconds(result.getEndMillis() - result.getStartMillis())) + "s";
+		String timeToRun = timeStartRun + " Take time: "
+				+ String.valueOf(TimeUnit.MILLISECONDS.toSeconds(result.getEndMillis() - result.getStartMillis()))
+				+ "s";
 		try {
 			if (result.getStatus() == ITestResult.SUCCESS) {
-				testResultData.put(method.getTestMethod().getMethodName(), "PASSED");
-				exceptionResult.put(method.getTestMethod().getMethodName(), "");
-				timeToRunResult.put(method.getTestMethod().getMethodName(), timeToRun);
+				dataAfterTest.add("PASSED");
+				dataAfterTest.add(result.getTestContext().getName());
+				dataAfterTest.add("");
+				dataAfterTest.add(timeToRun);
+				allDataAfterTest.put(method.getTestMethod().getMethodName(), dataAfterTest);
+
 			} else if (result.getStatus() == ITestResult.FAILURE) {
-				testResultData.put(method.getTestMethod().getMethodName(), "FAILED");
-				exceptionResult.put(method.getTestMethod().getMethodName(), result.getThrowable().getMessage());
-				timeToRunResult.put(method.getTestMethod().getMethodName(), timeToRun);
+				dataAfterTest.add("FAILED");
+				dataAfterTest.add(result.getTestContext().getName());
+				dataAfterTest.add(result.getThrowable().getMessage());
+				dataAfterTest.add(timeToRun);
+				allDataAfterTest.put(method.getTestMethod().getMethodName(), dataAfterTest);
+
 			} else if (result.getStatus() == ITestResult.SUCCESS_PERCENTAGE_FAILURE) {
-				testResultData.put(method.getTestMethod().getMethodName(), "FAILED");
-				exceptionResult.put(method.getTestMethod().getMethodName(), result.getThrowable().getMessage());
-				timeToRunResult.put(method.getTestMethod().getMethodName(), timeToRun);
+				dataAfterTest.add("FAILED");
+				dataAfterTest.add(result.getTestContext().getName());
+				dataAfterTest.add(result.getThrowable().getMessage());
+				dataAfterTest.add(timeToRun);
+				allDataAfterTest.put(method.getTestMethod().getMethodName(), dataAfterTest);
 			}
 		} catch (Exception e) {
-			System.out.println("\nLog Message::@AfterMethod: Exception caught");
+			log.info("\nLog Message::@AfterMethod: Exception caught");
 			e.printStackTrace();
 		}
 
-		setCellData(testResultData, exceptionResult, timeToRunResult);
+		setCellData();
 	}
 
 	@Override
 	public void beforeInvocation(IInvokedMethod method, ITestResult result) {
 		// TODO Auto-generated method stub
-
 	}
 
-	private void setCellData(Map<String, String> testResultData, Map<String, String> exceptionResult,
-			Map<String, String> timeToRunResult) {
+	private void setCellData() {
 		try {
 			FileInputStream file = new FileInputStream(new File(Constant.reportFilePath));
 			excelBook = new HSSFWorkbook(file);
 			excelSheet = excelBook.getSheet("Test");
-			for (int i = 0; i <= excelSheet.getLastRowNum(); i++) {
+			int lastRowNum = excelSheet.getLastRowNum();
+			// Read row Excel
+			for (int i = 1; i <= lastRowNum; i++) {
+				log.info("row " + i);
+				// Set cell
+				String testCaseName = excelSheet.getRow(i).getCell(0).getStringCellValue();
+				Cell platformCellValue = excelSheet.getRow(i).getCell(1);
 				Cell resultCellValue = excelSheet.getRow(i).getCell(2);
 				Cell exceptionCell = excelSheet.getRow(i).getCell(3);
 				Cell timeToRunCell = excelSheet.getRow(i).getCell(4);
-				String testCaseName = excelSheet.getRow(i).getCell(0).getStringCellValue();
-				// Write test result into Excel
-				for (Map.Entry<String, String> entry : testResultData.entrySet()) {
+				// Read dataAfterTest
+				for (Map.Entry<String, ArrayList<String>> entry : allDataAfterTest.entrySet()) {
 					if (entry.getKey().equals(testCaseName)) {
-						resultCellValue.setCellValue(entry.getValue());
-					}
-				}
-				// Write cause by into Excel
-				for (Map.Entry<String, String> entry : exceptionResult.entrySet()) {
-					if (entry.getKey().equals(testCaseName)) {
-						exceptionCell.setCellValue(entry.getValue());
-					}
-				}
-				// Write time to run into Excel
-				for (Map.Entry<String, String> entry : timeToRunResult.entrySet()) {
-					if (entry.getKey().equals(testCaseName)) {
-						timeToRunCell.setCellValue(entry.getValue());
+						if (platformCellValue.getStringCellValue().equals("")) {
+							log.info("platformCell is empty");
+							// Set excel values
+							resultCellValue.setCellValue(entry.getValue().get(0));
+							platformCellValue.setCellValue(entry.getValue().get(1));
+							exceptionCell.setCellValue(entry.getValue().get(2));
+							timeToRunCell.setCellValue(entry.getValue().get(3));
+						}
+						log.info("platformCell is used");
+						// Insert new row between
+						addNewRow(excelSheet, i, lastRowNum);
+						log.info("Add Row");
 					}
 				}
 			}
-
 			FileOutputStream fileOut = new FileOutputStream(new File(Constant.reportFilePath));
 			excelBook.write(fileOut);
 			fileOut.flush();
 			fileOut.close();
+
 		} catch (Exception e) {
 			System.out.print(e.getMessage());
 		}
-
 	}
+
+	private void addNewRow(HSSFSheet excelSheet, int rowIndex, int lastRowNum) {
+		// TODO Auto-generated method stub
+		if (rowIndex >= 0 && rowIndex < lastRowNum) {
+			excelSheet.shiftRows(rowIndex + 1, lastRowNum, 1);
+		}
+		if (rowIndex == lastRowNum) {
+			Row addRow = excelSheet.getRow(rowIndex);
+			if (addRow != null) {
+				excelSheet.createRow(addRow.getRowNum());
+			}
+		}
+	}
+
 	private String getTime(long millis) {
 		Calendar calendar = Calendar.getInstance();
 		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
